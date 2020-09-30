a4818de2fcb97650484835124e5023ca4abb5078
RacingPlayz <72081031+Racing-Playz@users.noreply.github.com>
2020-09-30 05:36:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,92 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
  
+client.on('message',async message => {
+    const moment = require('moment'); //npm i moment
+const ms = require('ms') //npm i ms
+    var prefix = '' //Bot Prefix !
+  var time = moment().format('Do MMMM YYYY , hh:mm');
+  var room;
+  var title;
+  var duration;
+  var currentTime = new Date(),
+hours = currentTime.getHours() + 3 ,
+minutes = currentTime.getMinutes(),
+done = currentTime.getMinutes() + duration,
+seconds = currentTime.getSeconds();
+if (minutes < 10) {
+minutes = "0" + minutes;
+}
+var suffix = "AM";
+if (hours >= 12) {
+suffix = "PM";
+hours = hours - 12;
+}
+if (hours == 0) {
+hours = 12;
+}
  
-const token = '';
+  var filter = m => m.author.id === message.author.id;
+  if(message.content.startsWith(prefix + "gstart")) {
  
-const PREFIX = '!';
+    if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **يجب أن يكون لديك خاصية التعديل على السيرفر**');
+    message.channel.send(`:eight_pointed_black_star:| **Send Name channel For the Giveaway**`).then(msg => {
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 20000,
+        errors: ['time']
+      }).then(collected => {
+        let room = message.guild.channels.find('name' , collected.first().content);
+        if(!room) return message.channel.send(':heavy_multiplication_x:| **i Found It :(**');
+        room = collected.first().content;
+        collected.first().delete();
+        msg.edit(':eight_pointed_black_star:| **Time For The Giveaway**').then(msg => {
+          message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 20000,
+            errors: ['time']
+          }).then(collected => {
+            if(!collected.first().content.match(/[1-60][s,m,h,d,w]/g)) return message.channel.send('**The Bot Not Support This Time**');
+            duration = collected.first().content
+            collected.first().delete();
+            msg.edit(':eight_pointed_black_star:| **Now send The Present **').then(msg => {
+              message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 20000,
+                errors: ['time']
+              }).then(collected => {
+                title = collected.first().content;
+                collected.first().delete();
+                msg.delete();
+                message.delete();
+                try {
+                  let giveEmbed = new Discord.RichEmbed()
+                  .setDescription(`**${title}** \nReact With 🎉 To Enter! \nTime remaining : ${duration} \n **Created at :** ${hours}:${minutes}:${seconds} ${suffix}`)
+                  .setFooter(message.author.username, message.author.avatarURL);
+                  message.guild.channels.find("name" , room).send(' :heavy_check_mark: **Giveaway Created** :heavy_check_mark:' , {embed: giveEmbed}).then(m => {
+                     let re = m.react('🎉');
+                     setTimeout(() => {
+                       let users = m.reactions.get("🎉").users
+                       let list = users.array().filter(u => u.id !== m.author.id !== client.user.id);
+                       let gFilter = list[Math.floor(Math.random() * list.length) + 0]
+                       let endEmbed = new Discord.RichEmbed()
+                       .setAuthor(message.author.username, message.author.avatarURL)
+                       .setTitle(title)
+                       .addField('Giveaway Ended !🎉',`Winners : ${gFilter} \nEnded at :`)
+                       .setTimestamp()
+                     m.edit('** 🎉 GIVEAWAY ENDED 🎉**' , {embed: endEmbed});
+                    message.guild.channels.find("name" , room).send(`**Congratulations ${gFilter}! You won The \`${title}\`**` , {embed: {}})
+                }, ms(duration));
+            });
+                } catch(e) {
+                message.channel.send(`:heavy_multiplication_x:| **i Don't Have Prem**`);
+                  console.log(e);
+                }
+              });
+            });
+          });
+        });
+      });
+    });
  
- 
-bot.on('ready', () => {
-    console.log('This bot is active!');
-})
- 
-bot.on('message', message => {
-    let args = message.content.substring(PREFIX.length).split(" ");
- 
-    switch (args[0]) {
-        case 'mute':
-            var person  = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
-            if(!person) return  message.reply("I CANT FIND THE USER " + person)
- 
-            let mainrole = message.guild.roles.find(role => role.name === "Newbie");
-            let role = message.guild.roles.find(role => role.name === "mute");
-           
- 
-            if(!role) return message.reply("Couldn't find the mute role.")
- 
- 
-            let time = args[2];
-            if(!time){
-                return message.reply("You didnt specify a time!");
-            }
- 
-            person.removeRole(mainrole.id)
-            person.addRole(role.id);
- 
- 
-            message.channel.send(`@${person.user.tag} has now been muted for ${ms(ms(time))}`)
- 
-            setTimeout(function(){
-                
-                person.addRole(mainrole.id)
-                person.removeRole(role.id);
-                console.log(role.id)
-                message.channel.send(`@${person.user.tag} has been unmuted.`)
-            }, ms(time));
- 
- 
-    
-        break;
-    }
- 
- 
-});
- 
-            
-                        
-bot.login('NzYwNDY4MzkzNzQ4MzMyNTc2.X3MfYQ.xpAAKiS3EhfYjyrljXWsXsT_zQY');
+client.login('NzYwNDY4MzkzNzQ4MzMyNTc2.X3MfYQ.xpAAKiS3EhfYjyrljXWsXsT_zQY');
